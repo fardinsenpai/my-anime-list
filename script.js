90ec8984abf41f909f1b46cee2345091a0d87704
fardinsenpai <nafisatasfin0@gmail.com>
2026-06-14 13:29:05 +0600

    Update model name in API request

--- script.js
+++ script.js
@@ -1923,7 +1923,7 @@ async function sendMessage() {
     'Content-Type': 'application/json'
   },
       body: JSON.stringify({
-        model: 'llama3-70b-8192',
+        model: 'llama-3.3-70b-versatile',
         messages: messagesToSend,
         temperature: 0.8,
         max_tokens: 400,
